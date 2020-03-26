83f70ca9335b007a5753994a0770d0a706f3f8e2
Christopher Persons <christopher.persons@rallyhealth.com>
2020-03-25 20:18:01 -0700

    Working post fix. . . maybe?

--- postfix-calculator/src/main.rs
+++ postfix-calculator/src/main.rs
@@ -1,11 +1,14 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-#[macro_use] extern crate rocket;
-#[macro_use] extern crate rocket_contrib;
-#[macro_use] extern crate serde_derive;
+#[macro_use]
+extern crate rocket;
+#[macro_use]
+extern crate rocket_contrib;
+#[macro_use]
+extern crate serde_derive;
 
 use std::sync::Mutex;
-use std::vec::Vec;
+use std::collections::VecDeque;
 
 use rocket::State;
 use rocket_contrib::json::{Json, JsonValue};
@@ -14,17 +17,16 @@ use rocket_contrib::json::{Json, JsonValue};
 type ID = usize;
 
 // We're going to store all of the messages here. No need for a DB.
-type Calculator = Mutex<Vec<String>>;
+type Calculator = Mutex<VecDeque<String>>;
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Message {
-    contents: Vec<String>
+    contents: VecDeque<String>
 }
 
 #[put("/push", format = "json", data = "<message>")]
 fn append(message: Json<Message>, calculator: State<Calculator>) -> JsonValue {
     let mut mut_calculator = calculator.lock().unwrap();
-    println!("{:?}", message);
     if message.contents.len() < 3 {
         json!({
             "status": "error",
@@ -32,9 +34,8 @@ fn append(message: Json<Message>, calculator: State<Calculator>) -> JsonValue {
         })
     } else {
         for content in &message.contents {
-            mut_calculator.push(content.to_string());
+            mut_calculator.push_back(content.to_string());
         }
-        println!("{}", mut_calculator.len());
         json!({ "status": "ok" })
     }
 }
@@ -42,47 +43,71 @@ fn append(message: Json<Message>, calculator: State<Calculator>) -> JsonValue {
 #[get("/calculate", format = "json")]
 fn calculate(calculator: State<Calculator>) -> JsonValue {
     let mut mut_calculator = calculator.lock().unwrap();
-    let mut first = None;
-    let mut second = None;
-    while (mut_calculator.len() > 0) {
-        let temp_pop = mut_calculator.pop();
-        match temp_pop {
+    let mut mut_temp_stack = VecDeque::<String>::new();
+    while mut_calculator.len() > 0 {
+        let front_pop = mut_calculator.pop_front();
+        match front_pop {
             Some(popped) => {
-                if first == None {
-                    first = Some(popped);
-                } else if second == None {
-                    second = Some(popped);
+                if is_int(&popped) {
+                    mut_temp_stack.push_back(popped)
+                } else if is_operator(&popped) {
+                    if mut_temp_stack.len() > 1 {
+                        let second = mut_temp_stack.pop_back();
+                        let first = mut_temp_stack.pop_back();
+                        let result = action(
+                            first.unwrap().parse::<i64>().unwrap(),
+                            second.unwrap().parse::<i64>().unwrap(),
+                            &popped
+                        );
+                        mut_temp_stack.push_back(result.to_string());
+                    } else {
+                        println!("Temporary stack is size 1 or less.");
+                    }
                 } else {
-                    // Is there a way in rust to do .get() similar to scala, I did a None check above
-                    // and just want to grab these values
-                    let result = match String::as_ref(&popped) {
-                        "*" => first.unwrap().parse::<u32>().unwrap() * second.unwrap().parse::<u32>().unwrap(),
-                        "/" => first.unwrap().parse::<u32>().unwrap() / second.unwrap().parse::<u32>().unwrap(),
-                        "+" => first.unwrap().parse::<u32>().unwrap() + second.unwrap().parse::<u32>().unwrap(),
-                        "-" => first.unwrap().parse::<u32>().unwrap() - second.unwrap().parse::<u32>().unwrap(),
-                        _ => 0,
-                    };
-                    first = Some(result.to_string());
-                    second = None;
+                    println!("Not an int or an operator.");
                 }
             }
             None => println!("Nothing popped.")
         }
     }
-    let result = match first {
+    let result = match mut_temp_stack.pop_front() {
         Some(val) => val.to_string(),
-        None => "nothing".to_string(),
+        None => "Nothing to calculator on the stack".to_string(),
     };
     json!({ "status":  result})
+}
+
+fn action(first: i64, second: i64, operator: &String) -> i64 {
+    return match String::as_ref(&operator) {
+        "*" => first * second,
+        "/" => first / second,
+        "+" => first + second,
+        "-" => first - second,
+        // shouldn't hit this case as we verify with is_operator
+        _ => 0,
+    };
+}
+
+fn is_int(popped: &String) -> bool {
+    return match popped.parse::<i64>() {
+        Ok(parsed) => true,
+        Err(e) => false,
+    };
+}
+
+fn is_operator(popped: &String) -> bool {
+    return match String::as_ref(&popped) {
+        "*" | "/" | "+" | "-" => true,
+        _ => false,
+    };
 }
 
 #[get("/", format = "json")]
 fn trace(calculator: State<Calculator>) -> Json<Message> {
     let mut mut_calculator = calculator.lock().unwrap();
-    let mut mut_vec = Vec::<String>::new();
-    println!("{}", mut_calculator.len());
+    let mut mut_vec = VecDeque::<String>::new();
     for content in mut_calculator.iter() {
-        mut_vec.push(content.to_string());
+        mut_vec.push_back(content.to_string());
     }
     Json(Message {
         contents: mut_vec
@@ -101,7 +126,7 @@ fn rocket() -> rocket::Rocket {
     rocket::ignite()
         .mount("/", routes![append, calculate, trace])
         .register(catchers![not_found])
-        .manage(Mutex::new(Vec::<String>::new()))
+        .manage(Mutex::new(VecDeque::<String>::new()))
 }
 
 fn main() {
